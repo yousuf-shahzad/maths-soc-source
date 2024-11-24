835d264c43ad2af7e58ea279051372ade3c3752c
yousuf shahzad <yousufshahzadd@yahoo.com>
2024-11-24 23:08:08 +0000

    doesn't matter

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CKEDITOR_SERVE_LOCAL = True
+    CKEDITOR_HEIGHT = 400
+    
+    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app', 'static', 'uploads', "challenges")
+    CKEDITOR_FILE_UPLOADER = 'upload'
